247f6aa5a32e96e26fb0bad4574b3408483e1ac9
eunhyuncho <ouoyy6395@gmail.com>
2019-10-16 02:55:41 +0900

    x만큼 간격이 있는 n개의 숫자 풀이 수정

--- src/main/java/Programmers/Level1/Sol12954.java
+++ src/main/java/Programmers/Level1/Sol12954.java
@@ -15,14 +15,15 @@ import java.util.Arrays;
 
 public class Sol12954 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(2, 5)));
+        System.out.println(Arrays.toString(solution(10000000, 1000)));
     }
 
     public static long[] solution(int x, int n) {
         long[] answer = new long[n];
+        answer[0] = x;
 
-        for (int i = 0; i < n; i++) {
-            answer[i] = (x * (i + 1));
+        for (int i = 1; i < n; i++) {
+            answer[i] = x + answer[i - 1];
         }
 
         return answer;
