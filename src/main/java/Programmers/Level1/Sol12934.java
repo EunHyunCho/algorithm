eddefc002ca31ec78de48d227bf04f6884899c30
eunhyuncho <ouoyy6395@gmail.com>
2019-10-24 00:49:25 +0900

    정수 제곱근 판별 문제풀이

---
+++ src/main/java/Programmers/Level1/Sol12934.java
@@ -0,0 +1,26 @@
+package Programmers.Level1;
+
+/*
+    정수 제곱근 판별
+    문제 설명
+    임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+    n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+
+    제한 사항
+    n은 1이상, 50000000000000 이하인 양의 정수입니다.
+ */
+
+public class Sol12934 {
+    public static void main(String[] args) {
+        System.out.println(solution(121));
+    }
+
+    public static long solution(long n) {
+        long answer = 0;
+        double num = Math.sqrt(n);
+        long numLong = (long) num;
+
+        answer = num == numLong ? (long) Math.pow(num + 1, 2) : -1;
+        return answer;
+    }
+}
